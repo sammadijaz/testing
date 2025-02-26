aa535f5a446fa870a95c9d20d4bb3b03d9b33a03
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-02-27 00:15:55 +0500

    ternary operators and examples

--- 01-Iman Musa/javascript/app.js
+++ 01-Iman Musa/javascript/app.js
@@ -18,3 +18,27 @@ document.write(`<h2>${"Sam"[3]}</h2>`)
 // trick to always access last char of string
 let str = "121h281h212h1821jh1uihx1asdsaacf"
 console.log(str[str.length - 1]);
+
+// Ternary Operators
+
+// condition ? "runs if condition is true" : "runs if condition is false"
+
+let isLoggedIn = false;
+
+isLoggedIn ? console.log("You are logged in.") : console.log("You are not logged in");
+
+// another example
+function askAge(){
+
+let age = prompt("What is your age?");
+
+age >= 18 ? alert("You are eligible!") : alert("You are not eligible!");
+
+} 
+// askAge();
+
+// example with logical operators
+let grade = 61
+let year = 2022
+
+grade >= 60 && year <= 2022 ? console.log("You can join uni") : console.log("You can not join uni");
