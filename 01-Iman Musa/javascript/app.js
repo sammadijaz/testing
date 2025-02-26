893daa9ce83d7291998110cec2128838ce06cdd7
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-02-26 23:22:34 +0500

    string character access and stuff

--- 01-Iman Musa/javascript/app.js
+++ 01-Iman Musa/javascript/app.js
@@ -1,2 +1,14 @@
 document.querySelector("body").style.backgroundColor = "#262626"
 document.querySelector("body").style.color = "#fff"
+document.querySelector("body").style.fontFamily = "sans-serif"
+document.querySelector("body").style.margin = "50px"
+
+console.log("Sam"[0]);
+console.log("Sam"[1]);
+console.log("Sam"[2]);
+console.log("Sam"[3]);
+
+document.write(`<h2>${"Sam"[0]}</h2>`)
+document.write(`<h2>${"Sam"[1]}</h2>`)
+document.write(`<h2>${"Sam"[2]}</h2>`)
+document.write(`<h2>${"Sam"[3]}</h2>`)
