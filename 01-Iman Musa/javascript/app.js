3ec9be479be5eed96ab67039459afcd031fc71ad
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-02-27 01:18:03 +0500

    completed loop section with tasks

--- 01-Iman Musa/javascript/app.js
+++ 01-Iman Musa/javascript/app.js
@@ -42,3 +42,78 @@ let grade = 61
 let year = 2022
 
 grade >= 60 && year <= 2022 ? console.log("You can join uni") : console.log("You can not join uni");
+
+
+// loops
+
+// for
+// printing from 0 to 20 with for loop
+for (let i=0; i<11; i++){
+    console.log(`for loop: ${i}`);
+}
+
+console.log("for loop finished");
+
+
+// printing from 0 to 10 with while loop
+let num = 0;
+
+while(num < 10){
+    num++; // num = num + 1
+    console.log(`While loop: ${num}`);
+}
+
+console.log("while loop finished");
+
+
+// tasks
+
+// if the number is odd print"odd", else print"even"
+
+// let num1 = prompt("Enter a number number: ")
+
+// if(num1 % 2 === 0){
+    
+//     alert(`${num1} is Even`);
+// } else {
+//     alert(`${num1} is Odd`);
+// }
+
+// // print yes if num is divisible by 5
+
+// let num2 = prompt("Enter Your Number: ")
+
+// if (num2 % 5 === 0){
+//     alert(`${num2} is divisible by 5`)
+// } else {
+//     alert(`${num2} is not divisible by 5`)
+// }
+// I got the task wrong now doing the actual one
+
+// actual task:
+
+for(let i = 1; i < 16; i++){
+    
+    if (i % 5 === 0){
+        console.log(`${i} is divisble by 5, ASAP Frontend`);
+    }
+    else if(i % 2 === 0){
+        console.log(`${i} is even, Frontend`);
+    } 
+    
+    else if (i % 2 !== 0){
+        console.log(`${i} is odd`);
+    }
+
+}
+
+
+// another task
+
+// print all letters from "ASAP Frontend"
+
+let letters = "ASAP Frontend";
+
+for (let i = 0; i < letters.length; i++){
+    console.log(letters[i]);
+}
