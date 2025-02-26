f0dc57ee9ad660ab7e5ae1a6b47ca47809c50e3f
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-02-26 23:46:36 +0500

    comments plus trick to acces last char

--- 01-Iman Musa/javascript/app.js
+++ 01-Iman Musa/javascript/app.js
@@ -3,6 +3,7 @@ document.querySelector("body").style.color = "#fff"
 document.querySelector("body").style.fontFamily = "sans-serif"
 document.querySelector("body").style.margin = "50px"
 
+// String and accessing its characters
 console.log("Sam"[0]);
 console.log("Sam"[1]);
 console.log("Sam"[2]);
@@ -12,3 +13,8 @@ document.write(`<h2>${"Sam"[0]}</h2>`)
 document.write(`<h2>${"Sam"[1]}</h2>`)
 document.write(`<h2>${"Sam"[2]}</h2>`)
 document.write(`<h2>${"Sam"[3]}</h2>`)
+
+
+// trick to always access last char of string
+let str = "121h281h212h1821jh1uihx1asdsaacf"
+console.log(str[str.length - 1]);
