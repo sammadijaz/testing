d248c93a00c1e9dc8c6648197f7b2cfc30d7717a
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-03-02 00:42:56 +0500

    wrote first html code in App.jsx and understood the file structure etc

--- 01-Iman Musa/React/my-react-app/src/App.jsx
+++ 01-Iman Musa/React/my-react-app/src/App.jsx
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h1>Hello This is my writing html in react</h1>
+      <h1>Hello This is my writing html in react</h1>
+      <h1>Hello This is my writing html in react</h1>
+      
+      <h2>Can you see that?!</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequuntur quia sapiente quisquam itaque ipsam natus non magnam minima beatae maiores fugit, laudantium cumque aliquid incidunt illo, unde ipsa enim.</p>
+
+
     </>
   )
 }
