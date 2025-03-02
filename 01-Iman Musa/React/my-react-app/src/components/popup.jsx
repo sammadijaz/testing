8583cb9411518b2d0d6ca15648005f48b0608a99
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-03-03 03:12:25 +0500

    props task done

--- 01-Iman Musa/React/my-react-app/src/components/popup.jsx
+++ 01-Iman Musa/React/my-react-app/src/components/popup.jsx
@@ -1,10 +1,10 @@
-function Popup(){
+function Popup({ custom }){
     return (
         
         <>
         
         <div className="popup">
-            <span>Are you sure?</span>
+            <span>{ custom }</span>
             <div className="popup__btns">
                 <button className="popup__btn">Confirm</button>
                 <button className="popup__btn popup__btn--cancel">Cancel</button>
