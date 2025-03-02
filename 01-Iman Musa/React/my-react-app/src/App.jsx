b5396da65329683d8d1efd519a523b434100b296
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-03-03 01:50:05 +0500

    starting todo app

--- 01-Iman Musa/React/my-react-app/src/App.jsx
+++ 01-Iman Musa/React/my-react-app/src/App.jsx
@@ -4,17 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>Hello This is my writing html in react</h1>
-      <h1>Hello This is my writing html in react</h1>
-      <h1>Hello This is my writing html in react</h1>
-      
-      <h2>Can you see that?!</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequuntur quia sapiente quisquam itaque ipsam natus non magnam minima beatae maiores fugit, laudantium cumque aliquid incidunt illo, unde ipsa enim.</p>
+      <h1>HELLO</h1>
+
+
 
 
     </>
